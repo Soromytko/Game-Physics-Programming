adda387ce1c9ab7df770df2e3e1ecd6bd618f40f
Soromytko <NikitaSoromytko@gmail.com>
2023-12-02 14:52:44 +0300

    Try to add a realistic simulation of the solar system Add a simulation of the solar system issue: laba-02

---
+++ Assets/Laba2/Scripts/Editor/SolarSystemEditor.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SolarSystem))]
+public class SolarSystemEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        SolarSystem targetScriptObject = (SolarSystem)target;
+
+        EditorGUI.BeginChangeCheck();
+        targetScriptObject.AstronomicalUnit = EditorGUILayout.FloatField("AstronomicalUnit", targetScriptObject.AstronomicalUnit);
+        targetScriptObject.EarthRadius = EditorGUILayout.FloatField("EarthRadius", targetScriptObject.EarthRadius);
+        if (EditorGUI.EndChangeCheck())
+        {
+            targetScriptObject.UpdatePlanets();
+        }
+        
+        if (GUILayout.Button("Update Planets"))
+        {
+            targetScriptObject.UpdatePlanets();
+        }
+
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("_planets"), true);
+        serializedObject.ApplyModifiedProperties();
+
+    }
+}
